49ca9024521cc1c81adfa98564331d0cf43b266d
bruno <bruno@caffeine.co.il>
2016-11-30 16:40:37 +0000

    chore(imports/exports): Added js/scss imports/exports

--- src/index.js
+++ src/index.js
@@ -8,3 +8,4 @@ export { default as Ripple } from './Ripple';
 export { default as SvgIcon } from './SvgIcon';
 export { default as Button } from './Button';
 export { default as Paper } from './Paper';
+export { default as AppBar } from './AppBar';
