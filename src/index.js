e807a55aeaef1b11d6e397db6c29a862ac00db97
bruno <bruno@caffeine.co.il>
2016-12-04 22:42:57 +0000

    refactor(exports): Components and respective scss imports/exports

--- src/index.js
+++ src/index.js
@@ -8,4 +8,7 @@ export { default as Ripple } from './Ripple';
 export { default as SvgIcon } from './SvgIcon';
 export { default as Button } from './Button';
 export { default as Paper } from './Paper';
+export { default as Menu } from './Menu';
+export { default as MenuItem } from './Menu/MenuItem';
+export { default as DropDown } from './DropDown';
 export { default as AppBar } from './AppBar';
