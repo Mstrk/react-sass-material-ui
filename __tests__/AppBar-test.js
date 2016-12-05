254e2595173254b9040312fff48ce4856a03ed46
bruno <bruno@caffeine.co.il>
2016-12-05 16:39:01 +0000

    test(AppBar): Drop iconRight, add contentRight

--- __tests__/AppBar-test.js
+++ __tests__/AppBar-test.js
@@ -3,12 +3,12 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { AppBar, Button } from '../src';
 
-describe('<Paper />', () => {
+describe('<AppBar />', () => {
   const appBar = shallow(
     <AppBar
       zDepth={1}
       iconLeft={{ color: 'white' }}
-      iconRight={{ color: 'white' }}
+      contentRight={<span>Some Content</span>}
       title='Fancy Title'
     />
   );
@@ -18,16 +18,28 @@ describe('<Paper />', () => {
   });
 
   it('sould render 1x div with class tile', () => {
-    expect(appBar.find('.title').length).toEqual(1);
+    expect(appBar.find('.title')).toHaveLength(1);
   });
 
-  it('sould render 2x Button components', () => {
-    expect(appBar.find(Button).length).toEqual(2);
+  it('sould have a div with default class left-content', () => {
+    expect(appBar.find('.left-content')).toHaveLength(1);
+  });
+
+  it('sould have a div with default class right-content', () => {
+    expect(appBar.find('.right-content')).toHaveLength(1);
+  });
+
+  it('sould have default class appBar', () => {
+    expect(appBar.hasClass('appBar')).toEqual(true);
+  });
+
+  it('sould render 1x Button components', () => {
+    expect(appBar.find(Button)).toHaveLength(1);
   });
 
   const wrapper = shallow(<AppBar />);
 
   it('sould not render Buttons', () => {
-    expect(wrapper.find(Button).length).toEqual(0);
+    expect(wrapper.find(Button)).toHaveLength(0);
   });
 });
