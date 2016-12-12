4e0e5692c1d5646860638a660ff41c1fab1ff05c
bruno <bruno@caffeine.co.il>
2016-12-12 09:06:33 +0000

    chore(demo): card examples

--- demo/index.js
+++ demo/index.js
@@ -12,7 +12,8 @@ import {
   Paper,
   Menu,
   MenuItem,
-  DropDown } from '../src';
+  DropDown,
+  Card } from '../src';
 
 // Main Application Styles
 import './app.scss';
@@ -28,6 +29,12 @@ const paperStyles = {
   width: '100px',
   height: '100px',
   marginRight: '20px',
+};
+
+const cardStyles = {
+  marginTop: '20px',
+  marginRight: '20px',
+  width: '400px'
 };
 
 const text = 'Paper';
@@ -96,6 +103,156 @@ ReactDOM.render(
         <MenuItem>Item 1</MenuItem>
         <MenuItem>Item 2</MenuItem>
       </Menu>
+      <br />
+
+      <div>   
+        <Card
+          style={cardStyles}
+          img={{
+            src: '/assets/airplane.jpg',
+            parentStyle: { maxHeight: '160px' }
+          }}
+        >
+          <div
+            className='card-text'
+          >
+            <p>Lorem ipsum dolor sit amet, 
+            consectetur adipiscing elit. 
+            Aenean tristique orci a lacinia malesuada. 
+            Lorem ipsum dolor sit amet, 
+            consectetur adipiscing elit. 
+            Aenean tristique orci a lacinia malesuada.</p>
+          </div>
+        </Card>
+
+        <Card
+          style={cardStyles}
+          img={{
+            src: '/assets/airplane.jpg'
+          }}
+
+          actions={
+            <span>
+              <Button
+                type='flat'
+                color='accent'
+                text='action 1'
+              />
+              <Button
+                type='flat'
+                color='accent'
+                text='action 2'
+              />
+            </span>
+          }
+
+          extendableContent={ 
+            <div
+              className='card-text'
+            >
+              <p>Lorem ipsum dolor sit amet, 
+              consectetur adipiscing elit. 
+              Aenean tristique orci a lacinia malesuada.</p>
+              <p>Lorem ipsum dolor sit amet, 
+              consectetur adipiscing elit. 
+              Aenean tristique orci a lacinia malesuada.</p>
+              <p>Lorem ipsum dolor sit amet, 
+              consectetur adipiscing elit. 
+              Aenean tristique orci a lacinia malesuada.</p>
+              <p>Lorem ipsum dolor sit amet, 
+              consectetur adipiscing elit. 
+              Aenean tristique orci a lacinia malesuada.</p>
+            </div>
+          }
+        >
+          <span
+            className='card-title'
+          >
+            Lorem Ipsum
+          </span>
+          <span
+            className='card-subtitle'
+          >Dolor sit amet</span>
+        </Card>
+
+        <Card
+          style={cardStyles}
+          img={{
+            src: '/assets/airplane.jpg',
+            caption: 'Lorem Ipsum',
+            parentStyle: { maxHeight: '200px' }
+          }}
+
+          actions={
+            <span>
+              <Button
+                type='flat'
+                color='accent'
+                text='action 1'
+              />
+              <Button
+                type='flat'
+                color='accent'
+                text='action 2'
+              />
+            </span>
+          }
+        >
+          <span
+            className='card-subtitle'
+          >
+            Lorem Ipsum
+          </span>
+          <div
+            className='card-text'
+          >
+            <p>Lorem ipsum dolor sit amet, 
+            consectetur adipiscing elit. 
+            Aenean tristique orci a lacinia malesuada.</p>
+          </div>
+        </Card>
+
+        <Card
+          style={cardStyles}
+          img={{
+            src: '/assets/dog.jpg'
+          }}
+
+          actions={
+            <span>
+              <Button
+                type='flat'
+                color='accent'
+                text='action 1'
+              />
+              <Button
+                type='flat'
+                color='accent'
+                text='action 2'
+              />
+            </span>
+          }
+
+          small
+          actionsDivider
+        >
+          <span
+            className='card-title'
+          >
+            Lorem Ipsum
+          </span>
+          <span
+            className='card-subtitle'
+          >
+            Lorem Ipsum
+          </span>
+          <div
+            className='card-text'
+          >
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </Card>
+      </div>
     </div>
   </div>, document.getElementById('root')
 );
