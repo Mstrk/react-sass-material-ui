3dae5e93247577f6c76c61aba77c37d248a9e1b4
bruno <bruno@caffeine.co.il>
2016-12-14 11:04:14 +0000

    test(DropDown): lifecycle tests

--- __tests__/DropDown-test.js
+++ __tests__/DropDown-test.js
@@ -1,23 +1,63 @@
 /*eslint no-undef:0*/
 import React from 'react';
-import { shallow } from 'enzyme';
-import { DropDown, Menu } from '../src';
+import { mount, shallow } from 'enzyme';
+import { DropDown, Menu, MenuItem } from '../src';
+
+jest.useFakeTimers();
 
 describe('<DropDown />', () => {
-  const dropDown = shallow(
+  const dropDown = mount(
     <DropDown anchorEl={<span />}>
       <Menu>
-        <span />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
       </Menu>
     </DropDown>
   );
 
+  // TODO: this is not the right way,
+  // it should be dropDown.hasClass('dropDown'),
+  // need to change this test when enzyme paches
+  // this issue: https://github.com/airbnb/enzyme/pull/677
   it('sould have default class dropDown', () => {
-    expect(dropDown.hasClass('dropDown')).toEqual(true);
+    expect(dropDown.find('.dropDown')).toHaveLength(1);
   });
 
   it('sould have child div with class dropDown-menu', () => {
     expect(dropDown.find('.dropDown-menu')).toHaveLength(1);
     expect(dropDown.find('.dropDown-menu').type()).toEqual('div');
+  });
+
+  it('should set state open true when anchor is clicked', () => {
+    expect(dropDown.state().open).toEqual(false);
+    dropDown.find('span').simulate('click');
+
+    jest.runOnlyPendingTimers();
+    expect(dropDown.state().open).toEqual(true);
+  });
+
+  it('should add delay(number) to each child', () => {
+    for (let i = 1; i <= 5; i++) {
+      expect(dropDown.find(`.delay${i}`)).toHaveLength(1);
+    }
+
+    dropDown.unmount();
+  });
+
+  const dropDown2 = shallow(
+    <DropDown anchorEl={<span />}>
+      <span />
+      <span />
+      <span />
+    </DropDown>
+  );
+
+  it('should not add delay(number) to each child', () => {
+    for (let i = 1; i <= 3; i++) {
+      expect(dropDown2.find(`.delay${i}`)).toHaveLength(0);
+    }
   });
 });
