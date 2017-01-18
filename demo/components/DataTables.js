0f1ec2c85ea5ff0ebc7aac5eb6ecb7d9dc987be0
bruno <bruno@caffeine.co.il>
2017-01-18 12:40:15 +0000

    chore(demo): updated

--- demo/components/DataTables.js
+++ demo/components/DataTables.js
@@ -40,6 +40,41 @@ const fakeData = [
     header2: 'content 12',
     header3: 'content 13',
     header4: 'content 14'
+  },
+  {
+    id: '21',
+    header1: '4500-121',
+    header2: '21035',
+    header3: 'content 23',
+    header4: 'content 24'
+  },
+  {
+    id: '22',
+    header1: 'content 31',
+    header2: 'content 32',
+    header3: 'content 33',
+    header4: 'content 34'
+  },
+  {
+    id: '23',
+    header1: 'content 11',
+    header2: 'content 12',
+    header3: 'content 13',
+    header4: 'content 14'
+  },
+  {
+    id: '24',
+    header1: 'content 11',
+    header2: 'content 12',
+    header3: 'content 13',
+    header4: 'content 14'
+  },
+  {
+    id: '25',
+    header1: 'content 11',
+    header2: 'content 12',
+    header3: 'content 13',
+    header4: 'content 14'
   }
 ];
 
