9be081a63223ee3c897f0181ec6b2f74c0efa0aa
bruno <bruno@caffeine.co.il>
2017-01-22 03:26:14 +0000

    chore(demo): updated

--- demo/components/DataTables.js
+++ demo/components/DataTables.js
@@ -1,77 +1,76 @@
 import React, { Component } from 'react';
 import {
   Paper,
-  DataTable,
-  Button
+  DataTable
 } from '../../src';
 
 const fakeData = [
   {
     id: '21',
-    header1: '4500-12122222222222',
+    camelCased: '4500-12122222222222',
     header2: '2103522222222222222',
     header3: 'content 23222222222',
     header4: 'content 24222222222'
   },
   {
     id: '22',
-    header1: 'content 31',
+    camelCased: 'content 31',
     header2: 'content 32',
     header3: 'content 33',
     header4: 'content 34'
   },
   {
     id: '23',
-    header1: 'content 11',
+    camelCased: 'content 11',
     header2: 'content 12',
     header3: 'content 13',
     header4: 'content 14'
   },
   {
     id: '24',
-    header1: 'content 11',
+    camelCased: 'content 11',
     header2: 'content 12',
     header3: 'content 13',
     header4: 'content 14'
   },
   {
     id: '25',
-    header1: 'content 11',
+    camelCased: 'content 11',
     header2: 'content 12',
     header3: 'content 13',
     header4: 'content 14'
   },
   {
     id: '21',
-    header1: '4500-121',
+    camelCased: '4500-121',
     header2: '21035',
     header3: 'content 23',
     header4: 'content 24'
   },
   {
     id: '22',
-    header1: 'content 31',
+    camelCased: 'content 31',
     header2: 'content 32',
     header3: 'content 33',
     header4: 'content 34'
   },
   {
     id: '23',
-    header1: 'content 11',
+    camelCased: 'content 11',
     header2: 'content 12',
     header3: 'content 13',
     header4: 'content 14'
   },
   {
     id: '24',
-    header1: 'content 11',
+    camelCased: 'content 11',
     header2: 'content 12',
     header3: 'content 13',
     header4: 'content 14'
   },
   {
     id: '25',
-    header1: 'content 11',
+    camelCased: 'content 11',
     header2: 'content 12',
     header3: 'content 13',
     header4: 'content 14'
@@ -80,50 +79,31 @@ const fakeData = [
 
 class DataTables extends Component {
   state = {
-    itemsSelected: false
-  }
-
-  setItemsSelected = (data) => {
-    this.setState({ itemsSelected: data[0] });
+    data: fakeData
   }
 
   render() {
-    const { itemsSelected } = this.state;
-    
     return (
       <div style={{ padding: 'calc(64px + 2em) 2em' }}>
         <Paper
           zDepth={2}
           style={{ width: '100%' }}
         >
-          <div className='data-table-super-header'>
-            {!!itemsSelected &&
-              <span>{itemsSelected.header1} -- {itemsSelected.header2}$</span>
-            }
-            <span className='right'>
-              {!!itemsSelected &&
-                <Button type='icon' size='l' icon='arrow-right' color='accent' />
-              }
-            </span>
-          </div>
           <DataTable
+            maxHeight={450}
             headerLabels={[null, 'label header']}
             excludeKeys={['id', 'header3']}
-            sortableKeys={['header1', 'header2']}
-            data={fakeData}
+            sortableKeys={['camelCased', 'header2']}
+            data={this.state.data}
             dataMock={{
               id: '',
-              header1: '',
+              camelCased: '',
               header2: '',
               header3: '',
               header4: ''
             }}
             noDataMessage='Add a new item'
-            disabledRows={[fakeData[1]]}
-            withHeader={false}
-            withFooter
-            maxSelected={1}
-            onSelectedChange={this.setItemsSelected}
+            disabledRows={[this.state.data[1]]}
           />
         </Paper>
       </div>
