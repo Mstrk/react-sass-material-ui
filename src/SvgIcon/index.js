ae0d3a44d1a1846d70c4093d24a1ac81d9814495
bruno <bruno@caffeine.co.il>
2017-01-22 03:19:01 +0000

    feat(SvgIcon): added onClick callback

--- src/SvgIcon/index.js
+++ src/SvgIcon/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import paths from './iconPaths';
 
-export default ({ size, color, icon }) => (
-  <svg width={size} height={size} fill={color} viewBox="0 0 24 24">
+export default ({ size, color, icon, onClick }) => (
+  <svg width={size} height={size} fill={color} viewBox="0 0 24 24" onClick={onClick}>
     <path d={paths[icon]} />
   </svg>
 );
