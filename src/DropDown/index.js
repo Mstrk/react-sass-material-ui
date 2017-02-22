c93b6eabad2b9b4c046820ac517816f38b6f9a88
bruno <bruno@caffeine.co.il>
2017-02-22 14:28:00 +0000

    refactor(DropDown): more react way component

--- src/DropDown/index.js
+++ src/DropDown/index.js
@@ -1,61 +1,30 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
-import Menu from '../Menu';
 
 class DropDown extends Component {
   state = {
     open: false
   }
 
-  componentDidMount() {
-    document.addEventListener('mousedown', this.handleOutsideClick);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('mousedown', this.handleOutsideClick);
-  }
-
-  getDelayClasses(amount) {
-    let step = amount / 5;
-    const nextStep = step;
-    let delay = 1;
-    const classes = [];
-
-    for (let i = 1; i <= amount; i++) {
-      if (i <= step) {
-        classes.push(`delay${delay}`);
-      } else {
-        step += nextStep;
-        classes.push(`delay${++delay}`);
-      }
-    }
-
-    return classes;
-  }
-
   handleOutsideClick = () => {
-    if (!this.state.open) return;
     this.setState({ open: false });
   }
 
   handleClick = () => {
     setTimeout(() => {
       this.setState({ open: true });
-    }, 350);
+    }, 50);
   }
 
   render() {
     const { anchorEl, children } = this.props;
 
-    let cloned;
-    if (children.type === Menu) {
-      cloned = React.cloneElement(children, {
-        childClassNames: this.getDelayClasses(children.props.children.length)
-      });
-    }
-
     return (
-      <div className='dropDown'>
+      <div
+        tabIndex={0}
+        onBlur={this.handleOutsideClick}
+        className='dropDown'
+      >
         {React.cloneElement(anchorEl, {
           onClick: this.handleClick
         })}
@@ -67,7 +36,7 @@ class DropDown extends Component {
             )
           }
         >
-        {cloned || children}
+        {children}
         </div>
       </div>
     );
