2376ad8508a86cc20ebfb4e406de46efec739dfe
Bruno Morgado <mstrk512@gmail.com>
2017-06-06 14:34:17 +0100

    test(TextField): updated

--- __tests__/TextField-test.js
+++ __tests__/TextField-test.js
@@ -1,11 +1,10 @@
-/*eslint no-undef:0*/
-import React from 'react';
-import { mount, shallow } from 'enzyme';
-import { TextField } from '../src';
+import React from 'react'
+import { mount, shallow } from 'enzyme'
+import { TextField } from '../src'
 
 describe('<TextField />', () => {
-  const mockChange = jest.fn();
-  const mockFormat = x => (x);
+  const mockChange = jest.fn()
+  const mockFormat = x => (x)
 
   const input = mount(
     <TextField
@@ -16,48 +15,48 @@ describe('<TextField />', () => {
       format={mockFormat}
       allowClear
     />
-  );
+  )
 
   it('should have root div with class text-field-root', () => {
-    expect(input.find('.text-field-root')).toHaveLength(1);
-  });
+    expect(input.find('.text-field-root')).toHaveLength(1)
+  })
 
   it('should have div with class text-field', () => {
-    expect(input.find('.text-field')).toHaveLength(1);
-  });
+    expect(input.find('.text-field')).toHaveLength(1)
+  })
 
   it('should have div with default class text-field-color-primary', () => {
-    expect(input.find('.text-field-color-primary')).toHaveLength(1);
-  });
+    expect(input.find('.text-field-color-primary')).toHaveLength(1)
+  })
 
   it('should set focused to true on focus', () => {
-    expect(input.state().focused).toEqual(false);
-    input.find('input').simulate('focus');
-    expect(input.state().focused).toEqual(true);
-  });
+    expect(input.state().focused).toEqual(false)
+    input.find('input').simulate('focus')
+    expect(input.state().focused).toEqual(true)
+  })
 
   it('should set focused to false on blur', () => {
-    input.find('input').simulate('blur');
-    expect(input.state().focused).toEqual(false);
-  });
+    input.find('input').simulate('blur')
+    expect(input.state().focused).toEqual(false)
+  })
 
   it('should not set focused to false on blur if value', () => {
-    input.find('input').simulate('focus');
-    input.setProps({ value: 'foo' });
-    input.find('input').simulate('blur');
-    expect(input.state().focused).toEqual(true);
-  });
+    input.find('input').simulate('focus')
+    input.setProps({ value: 'foo' })
+    input.find('input').simulate('blur')
+    expect(input.state().focused).toEqual(true)
+  })
 
   it('should call handleChange', () => {
-    input.find('input').simulate('change');
-    expect(mockChange).toHaveBeenCalled();
-  });
+    input.find('input').simulate('change')
+    expect(mockChange).toHaveBeenCalled()
+  })
 
   it('should clear the value on clear click', () => {
-    input.find('input').simulate('focus');
-    input.find('.clear').simulate('click');
-    expect(mockChange.mock.calls.length).toEqual(2);
-  });
+    input.find('input').simulate('focus')
+    input.find('.clear').simulate('click')
+    expect(mockChange.mock.calls.length).toEqual(2)
+  })
 
   const inputDisabled = shallow(
     <TextField
@@ -65,13 +64,13 @@ describe('<TextField />', () => {
       onChange={noop => noop}
       disabled
     />
-  );
+  )
 
   it('shoud have class text-field-is-disabled', () => {
-    expect(inputDisabled.find('.text-field-is-disabled')).toHaveLength(1);
-  });
+    expect(inputDisabled.find('.text-field-is-disabled')).toHaveLength(1)
+  })
 
-  const mockChange2 = jest.fn();
+  const mockChange2 = jest.fn()
 
   const textarea = mount(
     <TextField
@@ -80,19 +79,19 @@ describe('<TextField />', () => {
       onChange={mockChange2}
       allowClear
     />
-  );
+  )
 
   it('should call handleChange', () => {
-    textarea.setProps({ value: 'bar' });
-    textarea.find('textarea').simulate('change');
-    expect(mockChange2).toHaveBeenCalled();
-  });
+    textarea.setProps({ value: 'bar' })
+    textarea.find('textarea').simulate('change')
+    expect(mockChange2).toHaveBeenCalled()
+  })
 
   it('should call handleChange on clear click', () => {
-    textarea.find('textarea').simulate('focus');
-    textarea.find('.clear').simulate('click');
-    expect(mockChange2.mock.calls.length).toEqual(2);
-  });
+    textarea.find('textarea').simulate('focus')
+    textarea.find('.clear').simulate('click')
+    expect(mockChange2.mock.calls.length).toEqual(2)
+  })
 
   const inputIcon = mount(
     <TextField
@@ -104,18 +103,18 @@ describe('<TextField />', () => {
       limit={10}
       helpMessage='foo'
     />
-  );
+  )
 
   it('should have class icon-container, icon-color-primary and limit', () => {
-    expect(inputIcon.find('.icon-container')).toHaveLength(1);
-    expect(inputIcon.find('.icon-color-primary')).toHaveLength(1);
-    expect(inputIcon.find('.limit')).toHaveLength(1);
-  });
+    expect(inputIcon.find('.icon-container')).toHaveLength(1)
+    expect(inputIcon.find('.icon-color-primary')).toHaveLength(1)
+    expect(inputIcon.find('.limit')).toHaveLength(1)
+  })
 
   it('should have limit and error classes', () => {
-    inputIcon.setProps({ error: true, limitError: true });
-    expect(inputIcon.find('.icon-color-red')).toHaveLength(1);
-    expect(inputIcon.find('.text-field-color-red')).toHaveLength(1);
-    expect(inputIcon.find('.help-message-red')).toHaveLength(1);
-  });
-});
+    inputIcon.setProps({ error: true, limitError: true })
+    expect(inputIcon.find('.icon-color-red')).toHaveLength(1)
+    expect(inputIcon.find('.text-field-color-red')).toHaveLength(1)
+    expect(inputIcon.find('.help-message-red')).toHaveLength(1)
+  })
+})
